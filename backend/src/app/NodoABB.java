1875741570621272347d06e60a7dce0f4673f2ad
mathiuxd <matzap.ai@gmail.com>
2025-11-12 13:45:52 -0500

    Actualizacion x2

--- backend/src/app/NodoABB.java
+++ backend/src/app/NodoABB.java
@@ -1,5 +1,40 @@
 package app;
 
 public class NodoABB {
+    Estudiante estudiante;
+    NodoABB izquierda;
+    NodoABB derecha;
+
+    public NodoABB(Estudiante estudiante, NodoABB izquierda, NodoABB derecha) {
+        this.estudiante = estudiante;
+        this.izquierda = null;
+        this.derecha = null;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public NodoABB getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(NodoABB izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public NodoABB getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(NodoABB derecha) {
+        this.derecha = derecha;
+    }
+
     
+
 }
